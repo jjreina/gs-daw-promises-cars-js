866b41bedfe4b119833628a4347a24fd63c50b9c
jjreina <jreinapascual@gmail.com>
2024-11-15 18:49:10 +0100

    Refactor mock data structure and implement asynchronous car data handling

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import "./style.css";
+import { cars } from "./data/mockData";
+import {
+  promiseParserToClass,
+  promiseFilterByYears,
+  promiseCreateDivCars,
+} from "./utils-promises";
 
 let containerDiv = document.createElement("div");
 containerDiv.classList.add("container");
@@ -9,3 +15,16 @@ h1.textContent = "Coches desde el 2010";
 containerDiv.appendChild(h1);
 
 document.body.appendChild(containerDiv);
+
+let getCars = async () => {
+  let carsClass = await promiseParserToClass(cars);
+  let carsfiltedByYear = await promiseFilterByYears(carsClass);
+  let carsDivs = await promiseCreateDivCars(carsfiltedByYear);
+
+  let divBlock = document.createElement("div");
+  divBlock.className = "block";
+  divBlock.append(...carsDivs);
+  containerDiv.appendChild(divBlock);
+};
+
+getCars();
